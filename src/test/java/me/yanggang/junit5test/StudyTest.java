c2798c556cff585eb009dff859fd0a85c41cb8c4
slamdunk7575 <slamdunk7575@gmail.com>
2020-06-12 00:26:43 +0900

    Junit5 시작하기 (BeforeAll, AfterAll, BeforeEach, AfterEach)

--- src/test/java/me/yanggang/junit5test/StudyTest.java
+++ src/test/java/me/yanggang/junit5test/StudyTest.java
@@ -1,15 +1,41 @@
 package me.yanggang.junit5test;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyTest {
 
     @Test
-    void create() {
+    void create1() {
         Study study = new Study();
         assertNotNull(study);
+        System.out.println("Create1");
+    }
+
+    @Test
+    void create2() {
+        System.out.println("Create2");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
     }
 
 }
