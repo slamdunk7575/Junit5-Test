e89f5b6fd8c6579d759899a27c0580ca994ffc32
slamdunk7575 <slamdunk7575@gmail.com>
2020-06-12 23:44:29 +0900

    Junit5 시작하기 (DisplayNameGenerator, DisplayNameK)

--- src/test/java/me/yanggang/junit5test/StudyTest.java
+++ src/test/java/me/yanggang/junit5test/StudyTest.java
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    void create1() {
+    @DisplayName("스터디 만들기 (~˘▾˘)~ ")
+    void create_new_study_1() {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("Create1");
     }
 
     @Test
-    void create2() {
+    @DisplayName("스터디 만들기 \uD83E\uDD29")
+    void create_new_study_2() {
         System.out.println("Create2");
     }
 
