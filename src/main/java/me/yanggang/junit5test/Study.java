37e07bacb7a0662e764a753750d8c044027fd376
slamdunk7575 <slamdunk7575@gmail.com>
2020-06-15 00:31:29 +0900

    JUnit5 assertThrows 이용한 예외 메시지 확인

--- src/main/java/me/yanggang/junit5test/Study.java
+++ src/main/java/me/yanggang/junit5test/Study.java
@@ -2,9 +2,16 @@ package me.yanggang.junit5test;
 
 public class Study {
 
-  private StudyStatus status;
+  private StudyStatus status = StudyStatus.DRAFT;
 
   private int limit;
+
+  public Study(int limit) {
+    if(limit < 0) {
+      throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+    }
+    this.limit = limit;
+  }
 
   public int getLimit() {
     return limit;
--- src/test/java/me/yanggang/junit5test/StudyTest.java
+++ src/test/java/me/yanggang/junit5test/StudyTest.java
@@ -2,6 +2,7 @@ package me.yanggang.junit5test;
 
 import org.junit.jupiter.api.*;
 
+import javax.swing.text.Style;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,21 +13,33 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 (~˘▾˘)~ ")
     void create_new_study_1() {
-        Study study = new Study();
+        Study study = new Study(2);
 
         assertAll(
             () -> assertNotNull(study),
             () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-                () -> "Study를 처음 만들면 상태값이" +  StudyStatus.DRAFT + "여야 한다."),
+                () -> "Study를 처음 만들면 상태값은 " +  StudyStatus.DRAFT + " 여야 한다."),
             () -> assertTrue(study.getLimit() > 0, "스터디의 최대 참석 인원은 0보다 커야  한다.")
         );
     }
+
 
     @Test
     @DisplayName("스터디 만들기 \uD83E\uDD29")
     void create_new_study_2() {
         System.out.println("Create2");
     }
+
+    @Test
+    @DisplayName("예외 메시지 확인")
+    void create_new_study_3() {
+        IllegalArgumentException exception =
+            assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+        assertEquals("limit은 0보다 커야 한다.", message);
+    }
+
+
 
     @BeforeAll
     static void beforeAll() {
