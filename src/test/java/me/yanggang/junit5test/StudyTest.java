03dbeb53c40bc90ff6a502a33c8a4f62313a6d2e
slamdunk7575 <slamdunk7575@gmail.com>
2020-06-16 23:38:12 +0900

    조건에 따라 테스트 실행하기 (assumingThat)

--- src/test/java/me/yanggang/junit5test/StudyTest.java
+++ src/test/java/me/yanggang/junit5test/StudyTest.java
@@ -2,11 +2,13 @@ package me.yanggang.junit5test;
 
 import org.junit.jupiter.api.*;
 
+import javax.swing.text.Style;
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -31,6 +33,7 @@ class StudyTest {
         System.out.println("Create2");
     }
 
+
     @Test
     @DisplayName("예외 메시지 확인")
     void create_new_study_3() {
@@ -38,6 +41,7 @@ class StudyTest {
             assertThrows(IllegalArgumentException.class, () -> new Study(-10));
         assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
     }
+
 
     @Test
     @DisplayName("타임 아웃 테스트")
@@ -50,12 +54,14 @@ class StudyTest {
         // ThreadLocal을 사용하여 별개의 Thread로 테스트 코드를 실행하면 롤백이 안되고 DB에 반영되는 문제가 있음. 스프링 트랜잭션도 ThreadLocal을 기본전략으로 사용함.
     }
 
+
     @Test
     @DisplayName("Assertj 사용해보기")
     void create_new_study_5() {
         Study study = new Study(10);
         assertThat(study.getLimit()).isGreaterThan(0);
     }
+
 
     @Test
     @DisplayName("조건에 따라 테스트 실행하기")
@@ -68,6 +74,26 @@ class StudyTest {
 
         Study study = new Study(10);
         assertThat(study.getLimit()).isGreaterThan(0);
+    }
+
+
+    @Test
+    @DisplayName("조건에 따라 테스트 실행하기")
+    void create_new_study_7() {
+
+        String test_env = System.getenv("TEST_ENV");
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("LOCAL");
+            Study study = new Study(100);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
+
+        assumingThat("yanggang".equalsIgnoreCase(test_env), () -> {
+            System.out.println("yanggang");
+            Study study = new Study(10);
+            assertThat(study.getLimit()).isGreaterThan(0);
+        });
     }
 
 
