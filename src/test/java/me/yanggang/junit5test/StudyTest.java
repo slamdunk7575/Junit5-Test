3015ae6c07749f18e9466580f30b0fdfc57c97ef
slamdunk7575 <slamdunk7575@gmail.com>
2020-06-21 16:27:47 +0900

    JUnit5 테스트 인스턴스 (@TestInstance(TestInstance.Lifecycle.PER_CLASS))

--- src/test/java/me/yanggang/junit5test/StudyTest.java
+++ src/test/java/me/yanggang/junit5test/StudyTest.java
@@ -2,8 +2,12 @@ package me.yanggang.junit5test;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -18,7 +22,10 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StudyTest {
+
+    int value = 1;
 
     @FastTest
     @DisplayName("스터디 만들기 (~˘▾˘)~ ")
@@ -124,20 +131,44 @@ class StudyTest {
     // @ValueSource(ints = {10, 20, 30, 40})
     // @NullAndEmptySource
     @CsvSource({"10, '자바 스터디'", "20, 스프링"})
-    void parameterizedTest(ArgumentsAccessor argumentsAccessor) {
-        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
         System.out.println(study);
     }
 
+    // 여러 인자를 변환
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
+    }
+
+
     // 하나의 인자를 변환
     static class StudyConverter extends SimpleArgumentConverter {
-
         @Override
         protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
             assertEquals(Study.class, targetType, "Can Only Convert to Study");
             return new Study(Integer.parseInt(source.toString()));
         }
     }
+
+
+    @FastTest
+    @DisplayName("TEST 인스턴스 1")
+    void create_new_study_8() {
+        System.out.println(this);
+        System.out.println(value++);
+    }
+
+
+    @SlowTest
+    @DisplayName("TEST 인스턴스 2")
+    void create_new_study_9() {
+        System.out.println(this);
+        System.out.println(value++);
+    }
+
 
 
     @BeforeAll
