a27a869a190e9768516139e245e449256f614c04
slamdunk7575 <slamdunk7575@gmail.com>
2020-07-05 18:39:48 +0900

    Junit5 Mock 객체 Stubbing (doThrow())

--- src/test/java/me/yanggang/junit5test/study/StudyServiceTest.java
+++ src/test/java/me/yanggang/junit5test/study/StudyServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 
@@ -48,6 +49,14 @@ class StudyServiceTest {
         Optional<Member> findByIdMember = memberService.findById(1L);
         assertEquals("yanggang@email.com", memberService.findById(1L).get().getEmail());
         assertEquals("yanggang@email.com", memberService.findById(2L).get().getEmail());
+
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
+        memberService.validate(2L);
     }
 
 }
